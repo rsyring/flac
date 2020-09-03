fe486cd96901a8675d13e60c949f205ea395c331
Randy Syring <randy@thesyrings.us>
2020-09-02 23:10:50 -0400

    create error view

--- flac-app-cc/{{cookiecutter.project_folder}}/{{cookiecutter.project_namespace}}/views.py
+++ flac-app-cc/{{cookiecutter.project_folder}}/{{cookiecutter.project_namespace}}/views.py
@@ -6,3 +6,8 @@ public = Blueprint('public', __name__)
 @public.route('/')
 def index():
     return 'Hello World!'
+
+
+@public.route('/error')
+def error():
+    raise Exception('deliberate error')
