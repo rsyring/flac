92f24525565befff26919cf7ed6e47ea8e256850
Randy Syring <randy@thesyrings.us>
2023-01-16 00:50:04 -0500

    doc problems with dependencies

--- flac-app-cc/{{cookiecutter.project_folder}}/{{cookiecutter.project_pymod}}/ext.py
+++ flac-app-cc/{{cookiecutter.project_folder}}/{{cookiecutter.project_pymod}}/ext.py
@@ -1,6 +1,9 @@
 try:
     from flask_sqlalchemy import SQLAlchemy
 except ImportError:
+    # TODO: this doesn't work as expected.  flask_sqlalchemy is installed due to
+    # flac dependency even though setup.py says that should only be installed with an
+    # extra.
     class SQLAlchemy:
         def init_app(self, app):
             pass
